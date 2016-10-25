8c4c122ec194614da4a788ede3d28d17e20a5747
d_burke <omnidan@yahoo.com>
2016-10-24 20:25:21 -0700

    code clean up. Switch checkbox for div.

--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
-// Whenever checkbox is toggled, send message to feedkiller.js
-$(document).ready(function(){
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log('feedkiller.js loaded');
+//     var toggleFeedEval = "$('ol.feed.updates').toggle()";
+//     runMe(toggleFeedEval);
+//   });
 
-  // Whenever the checkbox changes, send a toggle event to the content script:
-  // $('#checkbox').change(function(){
-  //   alert('checkbox changed');
-  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //     chrome.tabs.sendMessage(tabs[0].id, {toggleFeed: true}, function(response) {
-  //       console.log('response from feedkiller.js:', response)
-  //     });
-  //   });
-  // });
+// Inject a script into tab window.
+function runMe (evalThis) {
+   alert('attempting to apply:  ' + evalThis);
+   chrome.tabs.executeScript(null, {code:evalThis});
+};
+
+// Toggling script to inject.
+var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
+
+
+var derp = runMe(toggleFeedEval);
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.getElementsByClassName('the-button');
+    divs[0].addEventListener('click', derp);
+  }
 });
