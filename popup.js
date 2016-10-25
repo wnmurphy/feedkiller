f5d49172a686404df36aaa12db5bc441705c0200
d-burke <omnidan@yahoo.com>
2016-10-25 12:36:56 -0700

    got it to work.

--- popup.js
+++ popup.js
@@ -6,18 +6,32 @@
 
 // Inject a script into tab window.
 function runMe (evalThis) {
-   alert('attempting to apply:  ' + evalThis);
-   chrome.tabs.executeScript(null, {code:evalThis});
+    //console.log('attempting to apply:  ' + evalThis);
+    // alert('attempting to apply:  ' + evalThis);
+    chrome.tabs.executeScript(null, {code:evalThis});
 };
 
 // Toggling script to inject.
 var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
 
 
-var derp = runMe(toggleFeedEval);
+// var derp = runMe(toggleFeedEval);
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var divs = document.getElementsByClassName('the-button');
+//     divs[0].addEventListener('click', derp);
+//   }
+// });
+
+
+
+//works!!
+function runIt(){
+    runMe(toggleFeedEval);
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.getElementsByClassName('the-button');
-    divs[0].addEventListener('click', derp);
-  }
+    document.getElementById('the-button').addEventListener('click', runIt);
+    //document.getElementsByClassName('the-button')[0].addEventListener('click', runIt);
 });
+
