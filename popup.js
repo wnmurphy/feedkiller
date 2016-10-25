a2f02ddd2177dc3bb80408f39ad5e07909d051a0
Neil Murphy <nmurphy@gopro.com>
2016-10-25 13:03:10 -0700

    Clean up code.

--- popup.js
+++ popup.js
@@ -1,37 +1,17 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     console.log('feedkiller.js loaded');
-//     var toggleFeedEval = "$('ol.feed.updates').toggle()";
-//     runMe(toggleFeedEval);
-//   });
+// Toggle script to inject.
+var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
 
-// Inject a script into tab window.
-function runMe (evalThis) {
-    //console.log('attempting to apply:  ' + evalThis);
-    // alert('attempting to apply:  ' + evalThis);
+// Inject the script into tab window.
+function inject (evalThis) {
     chrome.tabs.executeScript(null, {code:evalThis});
 };
 
-// Toggling script to inject.
-var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
-
-
-// var derp = runMe(toggleFeedEval);
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var divs = document.getElementsByClassName('the-button');
-//     divs[0].addEventListener('click', derp);
-//   }
-// });
-
-
-
-//works!!
-function runIt(){
-    runMe(toggleFeedEval);
+function injectWithScript () {
+    inject(toggleFeedEval);
 };
 
+// After DOM content loads, add an event listener to the button in popup.html.
+// Bind button to toggle behavior.
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('the-button').addEventListener('click', runIt);
-    //document.getElementsByClassName('the-button')[0].addEventListener('click', runIt);
+    document.getElementById('the-button').addEventListener('click', injectWithScript);
 });
-
