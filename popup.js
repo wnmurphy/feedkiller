adbf0c8a6d456d40b6abb308b49024b0a938969b
Neil Murphy <wnmurphy@gmail.com>
2016-10-26 19:58:17 -0700

    Store stuff

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // Toggle script to inject.
 var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
-var toggledOff = chrome.storage.sync.get('toggledOff') || false;
+var toggledOff = chrome.storage === undefined ?  false : chrome.storage.sync.get('toggledOff', null);
 
 // Inject the script into tab window.
 function inject (evalThis) {
@@ -19,7 +19,26 @@ document.addEventListener('DOMContentLoaded', function () {
       injectWithScript();
       //Save state 
       chrome.storage.sync.set({'toggledOff': !toggledOff}, function() {
-        console.log('Settings saved');
+       console.log('Settings saved');
       });
     });
 });
+
+
+// // Toggle script to inject.
+// var toggleFeedEval = "$('ol.feed.updates').toggle()"; 
+
+// // Inject the script into tab window.
+// function inject (evalThis) {
+//     chrome.tabs.executeScript(null, {code:evalThis});
+// };
+
+// function injectWithScript () {
+//     inject(toggleFeedEval);
+// };
+
+// // After DOM content loads, add an event listener to the button in popup.html.
+// // Bind button to toggle behavior.
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('the-button').addEventListener('click', injectWithScript);
+// });
