05602fbe714a0a026862c7cf1b00756fe0a3836b
Neil Murphy <wnmurphy@gmail.com>
2016-11-07 18:34:31 -0800

    Get/set to storage works

--- popup.js
+++ popup.js
@@ -1,41 +1,58 @@
 // Toggle script to inject.
 var toggleFeedEval = "$('ol.feed.updates').toggle()";
-
 var globalToggledOff;
 
-chrome.storage.local.set({"toggledOff" : false}, function(){
-  globalToggledOff = false;
-});
-console.log("globalToggledOff var in global, after initial set, is:");
-console.log(globalToggledOff);
 
-// var toggledOff = chrome.storage === undefined ?  false : chrome.storage.sync.get('toggledOff', null);
+// Set initial state of the toggle tracker.
+chrome.storage.sync.get("toggledOff", function(result){
+  // If the flag's never been initialized, initialize it.
+  if(result.toggledOff === undefined){
+    chrome.storage.sync.set({"toggledOff" : null});    
+  }
+});
+
 
 // Inject the script into tab window.
 function inject (evalThis) {
     chrome.tabs.executeScript(null, {code:evalThis});
 
-    // chrome.storage.local.get("toggledOff", function(data){
+    // chrome.storage.sync.get("toggledOff", function(data){
     //   toggledOff = data.toggledOff;
     // });
     console.log("globalToggledOff var in inject is:");
     console.log(globalToggledOff);
 };
 
-function injectWithScript () {
+
+function injectTheScript () {
     inject(toggleFeedEval);
 };
+
+
+function elementIsHiddenOnPage () {
+  if ( $('ol.feed.updates').css('display') === "none") {
+    return true;
+  }
+  return false;
+}
 
 // After DOM content loads, add an event listener to the button in popup.html.
 // Bind button to toggle behavior.
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Let's see what's getting set in storage.
+    chrome.storage.sync.get("toggledOff", function(result){
+      console.log("Getter gets:");
+      console.log(result.toggledOff);
+    });
+
     document.getElementById('the-button').addEventListener('click', function(){
 
       // Inject toggling script to browser tab.
-      injectWithScript();
+      injectTheScript();
 
       // Flip value of flag in both storage and global variable.
-      chrome.storage.local.set({"toggledOff" : !globalToggledOff}, function(){
+      chrome.storage.sync.set({"toggledOff" : !globalToggledOff}, function(){
         globalToggledOff = !globalToggledOff;
       });
 
--- scratchpad.js
+++ scratchpad.js
@@ -3,6 +3,10 @@
 
 
 
+
+
+
+// ==============================================================
 var derp;
 chrome.storage.local.set({"derp" : "B00p"});
 chrome.storage.local.get("derp", function(data){
@@ -15,7 +19,7 @@ console.log("what is derp: " + derp);
 
 
 
-
+// ==============================================================
 // Sample get/set usage
 chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
     //  A data saved callback omg so fancy
@@ -29,19 +33,19 @@ chrome.storage.sync.get(// String or Array //["yourBody"], function(items){
 
 
 
+
+// ==============================================================
 // Last good working copy before storage feature:
+
 // Toggle script to inject.
 var toggleFeedEval = "$('ol.feed.updates').toggle()";
-
 // Inject the script into tab window.
 function inject (evalThis) {
     chrome.tabs.executeScript(null, {code:evalThis});
 };
-
 function injectWithScript () {
     inject(toggleFeedEval);
 };
-
 // After DOM content loads, add an event listener to the button in popup.html.
 // Bind button to toggle behavior.
 document.addEventListener('DOMContentLoaded', function () {
